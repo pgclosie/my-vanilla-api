0db02d3da1824477205cd7cbb459f4a5400fdaf7
Patrick Close <pclose@nanthealth.com>
2021-04-14 10:27:20 +0000

    Adding message to new branch

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, req) {
     context.res = {
       body: {
-        text: "If you get here - MVF-1812 is complete - put your feet up and have a cuppa!"
+        text: "If you get here - you are on the wrong branch! :-)"
       }
     };
   };
